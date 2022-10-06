a5b831228d12d3f955ee6c5810483ce385983a07
Kiefer Weis <kieferweis@protonmail.com>
2022-10-05 22:34:09 -0400

    Added styling to restaurant.ejs

--- models/RestaurantComment.js
+++ models/RestaurantComment.js
@@ -4,7 +4,8 @@ const RestaurantCommentSchema = new mongoose.Schema({
     comment: {type:String, required:true},
     restaurantId: {type:mongoose.Schema.Types.ObjectId, ref:"Restaurant"},
     addedBy: {type:String, ref:"User"},
-    addedById: {type:mongoose.Schema.Types.ObjectId, ref:"User"}
+    addedById: {type:mongoose.Schema.Types.ObjectId, ref:"User"},
+    entryDate: {type:Date, default:Date.now},
 })
 
 module.exports = mongoose.model("RestaurantComment", RestaurantCommentSchema);
