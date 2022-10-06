d04baf7a632387ca6c649a2b131fc855becd65de
Kiefer Weis <kieferweis@protonmail.com>
2022-10-05 21:18:55 -0400

    Added styling for profile.ejs

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const RestaurantSchema = new mongoose.Schema({
   restaurantName: { type: String, required: true},
-  menuUrl: {type:String, require:true},
-  tag: {type:String},
+  menuUrl: {type:String, required:true},
+  tag: {type:String, required:true},
   addedBy: {type:String, ref:"User"},
   addedById: {type: mongoose.Schema.Types.ObjectId, ref:"User"},
   entryDate: {type:Date, default:Date.now},
